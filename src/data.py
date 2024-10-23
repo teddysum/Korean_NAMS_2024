a49e1aac94875236febcd544671547484acbbd3a
Yongbin Jeong <kuobon@naver.com>
2024-10-23 13:19:42 +0900

    Add files via upload

---
+++ src/data.py
@@ -0,0 +1,114 @@
+import json
+import torch
+from torch.utils.data import Dataset
+
+class CustomDataset(Dataset):
+    def __init__(self, fname, tokenizer, max_source_length=1024, max_target_length=512):
+        IGNORE_INDEX = -100
+        self.inp = []
+        self.label = []
+
+        PROMPT = '''You are a helpful AI assistant. Please answer the user's questions kindly. 당신은 유능한 AI 어시스턴트 입니다. 사용자의 질문에 대해 친절하게 답변해주세요.'''
+
+        with open(fname, "r") as f:
+            data = json.load(f)
+
+        def make_chat(inp):
+            chat = ["[Conversation]"]
+            keyword_sentence_id = inp['issue']['sentence_id']
+
+            # keyword_sentence 근처 40개의 대화만 포함
+            if len(inp['conversation']) > 40:
+                start_id = int(keyword_sentence_id.split('.')[-1]) - 20
+                if start_id < 0:
+                    start_id = 0
+                end_id = start_id + 40
+                if end_id > len(inp['conversation']):
+                    end_id = len(inp['conversation'])
+                    start_id = end_id - 40
+            else:
+                start_id = 0
+                end_id = len(inp['conversation'])
+
+            for cvt in inp['conversation'][start_id:end_id]:
+                speaker = cvt['speaker']
+                utterance = cvt['utterance']
+                chat.append(f"화자 {speaker}: {utterance}")
+            chat = "\n".join(chat)
+
+            question = f"[Question]\n위 대화에서 토픽 {', '.join(inp['issue']['topic'])}에 대한 대화를 요약해주세요."
+            chat = chat + "\n\n" + question
+
+            return chat
+
+        for example in data:
+            chat = make_chat(example["input"])
+            message = [
+                {"role": "system", "content": PROMPT},
+                {"role": "user", "content": chat},
+            ]
+
+            # source 텍스트에 대해 max_length 및 truncation 적용
+            source = tokenizer.apply_chat_template(
+                message,
+                add_generation_prompt=True,
+                return_tensors="pt",
+            )
+
+            # source의 길이 제한
+            source_truncated = tokenizer(
+                source['input_ids'][0],  # 입력 시퀀스
+                max_length=max_source_length,  # 입력의 최대 길이 제한
+                truncation=True,
+                padding="max_length",
+                return_tensors="pt"
+            )
+
+            # target 텍스트에 대한 처리
+            target = example["output"]
+            if target != "":
+                target += tokenizer.eos_token
+            target = tokenizer(
+                target,
+                return_attention_mask=False,
+                add_special_tokens=False,
+                return_tensors="pt",
+                max_length=max_target_length,  # target의 최대 길이 제한
+                truncation=True
+            )
+            target["input_ids"] = target["input_ids"].type(torch.int64)
+
+            # source와 target 병합
+            input_ids = torch.concat((source_truncated['input_ids'][0], target["input_ids"][0]))
+            labels = torch.concat((torch.LongTensor([IGNORE_INDEX] * source_truncated['input_ids'].shape[1]), target["input_ids"][0]))
+
+            # 최종 결과 추가
+            self.inp.append(input_ids)
+            self.label.append(labels)
+
+    def __len__(self):
+        return len(self.inp)
+
+    def __getitem__(self, idx):
+        return {
+            'input_ids': self.inp[idx],
+            'labels': self.label[idx]
+        }
+
+class DataCollatorForSupervisedDataset(object):
+    def __init__(self, tokenizer):
+        self.tokenizer = tokenizer
+
+    def __call__(self, instances):
+        input_ids, labels = tuple([instance[key] for instance in instances] for key in ("input_ids", "labels"))
+        input_ids = torch.nn.utils.rnn.pad_sequence(
+            [torch.tensor(ids) for ids in input_ids], batch_first=True, padding_value=self.tokenizer.pad_token_id
+        )
+        labels = torch.nn.utils.rnn.pad_sequence(
+            [torch.tensor(lbls) for lbls in labels], batch_first=True, padding_value=-100
+        )
+        return dict(
+            input_ids=input_ids,
+            labels=labels,
+            attention_mask=input_ids.ne(self.tokenizer.pad_token_id),
+        )
